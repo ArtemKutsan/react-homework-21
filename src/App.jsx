d0a38b07fa7df066fe89412fa0b124f72e522bce
Artem Kutsan <artemkutsan@gmail.com>
2026-04-13 19:48:16 +0200

    feat: обновлён App -  список вопросов, кнопка Submit, Result

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { submitQuestionnaire } from './features/questionnaire/questionnaireSlice.js';
+import Question from './components/Question.jsx';
+import Result from './components/Result.jsx';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  const questions = useSelector(
+    (state) => state.questionnaire.questions
+  );
+
+  const handleSubmit = () => {
+    dispatch(submitQuestionnaire());
+  };
+
   return (
     <main className="py-8">
       <div className="container max-w-4xl prose">
         <h1>React Homework 21</h1>
 
-        <h2></h2>
+        <h2>Questionnaire</h2>
 
+        {questions.map((question) => (
+          <Question key={question.id} questionId={question.id} />
+        ))}
+
+        <button onClick={handleSubmit}>Submit</button>
+
+        <Result />
       </div>
     </main>
   );
