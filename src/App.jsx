e4d1753c8248d57f0c03129bbbac2372f2b6dca9
Artem Kutsan <artemkutsan@gmail.com>
2026-04-13 19:16:50 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="py-8">
+      <div className="container max-w-4xl prose">
+        <h1>React Homework 21</h1>
+
+        <h2></h2>
+
+      </div>
+    </main>
+  );
+}
+
+export default App;
